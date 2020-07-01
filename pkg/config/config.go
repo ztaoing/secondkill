03a75b460131d73b39136c07182e52bb451e780c
tao <1216707759@qq.com>
2020-07-02 07:57:09 +0800

    分布式配置中心组件

---
+++ pkg/config/config.go
@@ -0,0 +1,113 @@
+/**
+* @Author:zhoutao
+* @Date:2020/7/1 上午11:13
+* @分布式配置中心接入组件
+ */
+
+package config
+
+import (
+	"fmt"
+	"github.com/go-kit/kit/log"
+	"github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/reporter/http"
+	"github.com/spf13/viper"
+	"net/http"
+	"os"
+	"secondkill/pkg/bootstrap"
+	"secondkill/pkg/discover"
+	"strconv"
+)
+
+const KConfigType = "CONFIG_TYPE"
+
+var Logger log.Logger
+
+func init() {
+	Logger := log.NewLogfmtLogger(os.Stderr)
+	Logger = log.With(Logger, "timestamp", log.DefaultTimestampUTC)
+	Logger = log.With(Logger, "caller", log.DefaultCaller)
+
+	viper.AutomaticEnv()
+	initDeault()
+	//加载远端配置
+	if err := LoadRemoteConfig(); err != nil {
+		Logger.Log("fail to load remote config", err)
+	}
+	if err := Sub("trace", &TraceConfig); err != nil {
+		Logger.Log("Fail to parse trace", err)
+	}
+
+	zipkinUrl := "http://" + TraceConfig.Host + ":" + TraceConfig.Port + TraceConfig.Url
+	Logger.Log("zipkin url", zipkinUrl)
+
+	//初始化zipkin配置
+	initTracer(zipkinUrl)
+}
+
+//设置配置文件类型
+func initDeault() {
+	viper.SetDefault(KConfigType, "yaml")
+}
+
+//加载远端配置
+func LoadRemoteConfig() (err error) {
+	//配置中心：服务发现
+	serviceInstance, err := discover.DiscoveryService(bootstrap.ConfigServerConfig.ID)
+	if err != nil {
+		return
+	}
+
+	configServer := "http://" + serviceInstance.Host + ":" + strconv.Itoa(serviceInstance.Port)
+	confAddr := fmt.Sprintf("%v/%v/%v-%v.%v",
+		configServer,
+		bootstrap.ConfigServerConfig.Label,
+		bootstrap.DiscoverConfig.ServiceName,
+		bootstrap.ConfigServerConfig.Profile,
+		viper.Get(KConfigType),
+	)
+	resp, err := http.Get(confAddr)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	//设置配置类型
+	viper.SetConfigType(viper.GetString(KConfigType))
+	if err := viper.ReadConfig(resp.Body); err != nil {
+		return
+	}
+	Logger.Log("Load config from:", confAddr)
+	return
+}
+
+func Sub(key string, value interface{}) error {
+	Logger.Log("配置文件的前缀为：", key)
+	sub := viper.Sub(key)
+	sub.AutomaticEnv()
+	sub.SetEnvPrefix(key)
+	return sub.Unmarshal(value)
+}
+
+func initTracer(zipkinUrl string) {
+	var (
+		err           error
+		useNoopTracer = (zipkinUrl == "")
+		reporter      = zipkinhttp.NewReporter(zipkinUrl)
+	)
+
+	defer reporter.Close()
+
+	zEndpoint, _ := zipkin.NewEndpoint(bootstrap.DiscoverConfig.ServiceName, bootstrap.DiscoverConfig.Port)
+	_, err = zipkin.NewTracer(
+		reporter, zipkin.WithLocalEndpoint(zEndpoint), zipkin.WithNoopTracer(useNoopTracer),
+	)
+	if err != nil {
+		Logger.Log("new Tracer failed:", err)
+		os.Exit(1)
+	}
+	if !useNoopTracer {
+		Logger.Log("tracer", "zipkin", "type", "native", "url", zipkinUrl)
+	}
+
+}
---
+++ pkg/config/configProperty.go
@@ -0,0 +1,125 @@
+/**
+* @Author:zhoutao
+* @Date:2020/7/1 下午1:54
+ */
+
+package config
+
+import (
+	"github.com/coreos/etcd/clientv3"
+	"github.com/go-redis/redis"
+	"github.com/samuel/go-zookeeper/zk"
+	"sync"
+)
+
+var (
+	Redis       RedisConf
+	Etcd        EtcdConf
+	SecKill     SecKillConf
+	MysqlConfig MysqlConf
+	TraceConfig TraceConf
+	Zk          ZookeeperConf
+)
+
+//redis配置
+type RedisConf struct {
+	RedisConn            *redis.Client //链接
+	Host                 string
+	Password             string
+	Db                   int
+	Proxy2layerQueueName string //队列名称
+	Layer2ProxyQueueName string //队列名称
+	IdBlackListHash      string //用户黑名单hash表
+	IpBlackListHash      string //IP黑名单hash表
+	IdBlackListQueue     string //用户黑名单队列
+	IpBlackListQueue     string //IP黑名单队列
+
+}
+
+//Etcd配置
+type EtcdConf struct {
+	EtcdConn          *clientv3.Client //链接
+	EtcdSecProductKey string           //商品键
+	Host              string
+}
+
+//秒杀商品配置信息
+type SecKillConf struct {
+	RedisConf *RedisConf //redis配置
+	EtcdConf  *EtcdConf  //EtcdP配置
+
+	CookieSecretKey string   //cookie秘钥
+	ReferWhiteList  []string //包名单
+
+	AccessLimitConf AccessLimitConf
+
+	RWBlackLock                  sync.RWMutex
+	WriteProxy2LayerGoroutineNum int
+	ReadProxy2LayerGoroutineNum  int
+
+	IPBlackMap map[string]bool
+	IDBlackMap map[string]bool
+
+	SecProductInfoMap map[int]*SecProductInfoConf
+
+	AppWriteToHandleGoroutineNum  int
+	AppReadFromHandleGoroutineNum int
+
+	CoreReadRedisGoroutineNum  int
+	CoreWriteRedisGoroutineNum int
+	CoreHandleGoroutineNum     int
+
+	AppWaitResultTimeOut int
+
+	CoreWaitResultTimeOut int
+
+	MaxRequestWaitTimeout int
+
+	SendToWriteChanTimeout int
+	SendToHandChanTimeout  int
+	TokenPassWd            string
+}
+
+//MySQL配置
+type MysqlConf struct {
+	Host string
+	Port string
+	User string
+	Pwd  string
+	Db   string
+}
+
+//Trace配置
+type TraceConf struct {
+	Host string
+	Port string
+	Url  string
+}
+
+//zookeeper配置
+type ZookeeperConf struct {
+	ZkConn        *zk.Conn
+	SecProductKey string //商品键
+}
+
+//商品信息配置
+type SecProductInfoConf struct {
+	ProductID         int                 `json:"product_id"`           //商品ID
+	StartTime         int64               `json:"start_time"`           //开始时间
+	EndTime           int64               `json:"end_time"`             //结束时间
+	Status            int                 `json:"status"`               //状态
+	Total             int                 `json:"total"`                //商品总数
+	Left              int                 `json:"left"`                 //商品剩余数量
+	OnePersonBuyLimit int                 `json:"one_person_buy_limit"` //单个用户购买数量限制
+	BuyRate           float64             `json:"buy_rate"`             //购买频率限制
+	SoldMaxlimit      int                 `json:"sold_maxlimit"`        //销售最大量
+	SecLimit          *srv_limit.SecLimit `json:"sec_limit"`            //限速控制
+}
+
+//访问限制
+type AccessLimitConf struct {
+	IPSecAccessLimit   int //IP每秒访问限制
+	UserSecAccessLimit int //用户每秒范文限制
+	IPMinAccessLimit   int //IP每分钟访问限制
+	UserMinAccessLimit int //用户每分钟访问限制
+}
