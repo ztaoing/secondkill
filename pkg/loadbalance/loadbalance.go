a0d9505a44513e008b4e1db2191ad8a83e468891
tao <1216707759@qq.com>
2020-07-02 09:09:42 +0800

    客户端负载均衡组件

--- pkg/loadbalance/loadbalance.go
+++ pkg/loadbalance/loadbalance.go
@@ -13,7 +13,7 @@ import (
 
 type LoadBalance interface {
 	//根据负载均衡策略从实例列表中选择一个实例
-	SelectService([]*common.ServiceInstance) *common.ServiceInstance
+	SelectService(serviceList []*common.ServiceInstance) (*common.ServiceInstance, error)
 }
 
 //完全随机策略
