4ecadae83aedb723fa200b357692fea6c1d023f5
tao <1216707759@qq.com>
2020-07-03 16:36:10 +0800

    令牌管理&jwt封装

--- oauth-service/service/token_service.go
+++ oauth-service/service/token_service.go
@@ -8,6 +8,7 @@ package service
 import (
 	"context"
 	"errors"
+	"github.com/dgrijalva/jwt-go"
 	"net/http"
 	"secondkill/oauth-service/model"
 )
@@ -20,6 +21,11 @@ type TokenGranter interface {
 	Grant(ctx context.Context, grantType string, client *model.ClientDetails, r *http.Request) (*model.OAuth2Token, error)
 }
 
+/**
+组成token字典
+注意：使用组合模式，使得不同的授权类型使用不同的tokenGrant接口实现结构体来生成访问令牌
+组合节点ComposeTokenGranter
+*/
 type CommposeTokenGranter struct {
 	//token授权:map[grantType]TokenGranter
 	TokenGrantDict map[string]TokenGranter
@@ -121,4 +127,134 @@ type TokenStore interface {
 	ReadRefreshToken(tokenValue string) (*model.OAuth2Token, error)
 	//根据令牌值获取令牌绑定的客户端和用户信息
 	ReadOAuth2DetailsForRefreshToken(tokenValue string) (*model.OAuth2Details, error)
+}
+
+//token增强
+type TokenEnhancer interface {
+	//组装Token信息
+	Enhance(oauth2Token *model.OAuth2Token, oauth2Details *model.OAuth2Details) (*model.OAuth2Token, error)
+	//从Token中还原信息
+	Extract(tokenValue string) (*model.OAuth2Token, *model.OAuth2Details, error)
+}
+
+/**
+令牌管理 + token增强
+*/
+type DefaultTokenService struct {
+	tokenStore    TokenStore
+	tokenEnhancer TokenEnhancer //token增强
+}
+
+func (d *DefaultTokenService) GetOAuth2DetailsByAccessToken(tokenValue string) (*model.OAuth2Details, error) {
+	panic("implement me")
+}
+
+func (d *DefaultTokenService) CreateAccessToken(oauth2Details *model.OAuth2Details) (*model.OAuth2Token, error) {
+	panic("implement me")
+}
+
+func (d *DefaultTokenService) RefreshAccessToken(refreshTokenValue string) (*model.OAuth2Token, error) {
+	panic("implement me")
+}
+
+func (d *DefaultTokenService) GetAccessToken(details *model.OAuth2Token) (*model.OAuth2Token, error) {
+	panic("implement me")
+}
+
+func (d *DefaultTokenService) ReadAccessToken(TokenValue string) (*model.OAuth2Token, error) {
+	panic("implement me")
+}
+
+func NewTokenService(tokenStore TokenStore, enhancer TokenEnhancer) TokenService {
+	return &DefaultTokenService{
+		tokenStore:    tokenStore,
+		tokenEnhancer: enhancer,
+	}
+}
+
+/**
+jsonWebToken 的token增强
+*/
+type JwtTokenEnhancer struct {
+	secretKey []byte
+}
+
+func (enhance *JwtTokenEnhancer) Enhance(oauth2Token *model.OAuth2Token, oauth2Details *model.OAuth2Details) (*model.OAuth2Token, error) {
+	panic("implement me")
+}
+
+func (enhance *JwtTokenEnhancer) Extract(tokenValue string) (*model.OAuth2Token, *model.OAuth2Details, error) {
+	panic("implement me")
+}
+
+//将令牌对应的用户信息和客户端信息写入到JWT的声明中
+func (enhance *JwtTokenEnhancer) sign(token *model.OAuth2Token, details *model.OAuth2Details) (*model.OAuth2Token, error) {
+
+}
+
+func NewJwtTokenEnhancer(secretKey string) TokenEnhancer {
+	return &JwtTokenEnhancer{
+		secretKey: []byte(secretKey),
+	}
+}
+
+/**
+jsonWebToken的令牌存储器
+*/
+type JwtTokenStore struct {
+	jwtTokenEnhancer *JwtTokenEnhancer
+}
+
+func (j *JwtTokenStore) StoreAccessToken(oauth2Token *model.OAuth2Token, oauth2Details *model.OAuth2Details) {
+	panic("implement me")
+}
+
+func (j *JwtTokenStore) ReadAccessToken(tokenValue string) (*model.OAuth2Token, error) {
+	panic("implement me")
+}
+
+func (j *JwtTokenStore) ReadOAuth2Details(tokenValue string) (*model.OAuth2Details, error) {
+	panic("implement me")
+}
+
+func (j *JwtTokenStore) GetAccessToken(oath2Details *model.OAuth2Details) (*model.OAuth2Token, error) {
+	panic("implement me")
+}
+
+func (j *JwtTokenStore) RemoveAccessToken(tokenValue string) {
+	panic("implement me")
+}
+
+func (j *JwtTokenStore) StoreRefreshToken(oauth2Token *model.OAuth2Token, oauth2Details *model.OAuth2Details) {
+	panic("implement me")
+}
+
+func (j *JwtTokenStore) RemoveRefreshToken(oauth2Token string) {
+	panic("implement me")
+}
+
+func (j *JwtTokenStore) ReadRefreshToken(tokenValue string) (*model.OAuth2Token, error) {
+	panic("implement me")
+}
+
+func (j *JwtTokenStore) ReadOAuth2DetailsForRefreshToken(tokenValue string) (*model.OAuth2Details, error) {
+	panic("implement me")
+}
+
+func NewJwtTokenStore(enhancer *JwtTokenEnhancer) TokenStore {
+	return &JwtTokenStore{
+		jwtTokenEnhancer: enhancer,
+	}
+}
+
+//声明信息
+type OAuth2TokenCustomClaims struct {
+	//用户信息
+	UserDetails model.UserDetails
+	//客户端信息
+	ClientDetails model.ClientDetails
+	//用于刷新的令牌
+	RefreshToken model.OAuth2Token
+	//jwt标准声明
+	jwt.StandardClaims
 }
