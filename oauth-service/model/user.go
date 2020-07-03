ab35abceaafd6ed2ff1afc70662f237c4bc800a9
tao <1216707759@qq.com>
2020-07-03 09:01:06 +0800

    用户信息

---
+++ oauth-service/model/user.go
@@ -0,0 +1,21 @@
+/**
+* @Author:zhoutao
+* @Date:2020/7/3 上午8:57
+ */
+
+package model
+
+type UserDetails struct {
+	//用户标识
+	UserId int64
+	//用户名 唯一
+	UserName string
+	//密码
+	Password string
+	//用户拥有的权限
+	Authoritied []string
+}
+
+func (userDetails *UserDetails) IsMatch(username string, password string) bool {
+	return userDetails.Password == password && userDetails.UserName == username
+}
