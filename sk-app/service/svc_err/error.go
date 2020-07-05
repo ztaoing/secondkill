a01dbdd8e4edc37f113582b196999cf84ad8d79e
tao <1216707759@qq.com>
2020-07-05 14:53:39 +0800

    定义错误编号

--- sk-app/service/svc_err/error.go
+++ sk-app/service/svc_err/error.go
@@ -5,3 +5,37 @@
  */
 
 package svc_err
+
+import "errors"
+
+const (
+	ErrServiceBusy      = 1001
+	SecKillSuccess      = 1002
+	ErrNostFoundProduct = 1003
+	ErrSoldOut          = 1004
+	ErrRetry            = 1005
+	ErrAlreadyBuy       = 1006
+
+	ErrInvalidRequest      = 1101
+	ErrNotFoundProductId   = 1102
+	ErrUserCheckAuthFailed = 1103
+	ErrUserServiceBusy     = 1104
+	ErrActiveNotStart      = 1105
+	ErrActiveAlreadyEnd    = 1106
+	ErrActiveSaleOut       = 1107
+	ErrProcessTimeout      = 1108
+	ErrClientClose         = 1109
+)
+
+var errMsg = map[int]string{
+	ErrServiceBusy:       "服务器错误",
+	SecKillSuccess:       "抢购成功",
+	ErrNotFoundProductId: "没有该商品",
+	ErrSoldOut:           "商品售完",
+	ErrRetry:             "请重试",
+	ErrAlreadyBuy:        "已抢购",
+}
+
+func GetErrMsg(code int) error {
+	return errors.New(errMsg[code])
+}
