ead957222b3c1253128b35dd8bcb05698c1cd26a
tao <1216707759@qq.com>
2020-07-05 22:16:54 +0800

    将请求写入redis队列中,从redis队列中读取数据

--- sk-app/service/svc_redis/redis.go
+++ sk-app/service/svc_redis/redis.go
@@ -11,6 +11,8 @@ import (
 	"log"
 	pkgConfig "secondkill/pkg/config"
 	"secondkill/sk-app/config"
+	"secondkill/sk-app/model"
+	"time"
 )
 
 //写数据到redis
@@ -47,6 +49,35 @@ func WriteHandler() {
 //从redis读取数据
 func ReadHandler() {
 	for {
+		conn := pkgConfig.Redis.RedisConn
+		//使用阻塞弹出
+		data, err := conn.BRPop(time.Second*1, pkgConfig.Redis.Proxy2layerQueueName).Result()
+		if err != nil {
+			continue
+		}
+		var result *model.SecResult
+		err = json.Unmarshal([]byte(data[1]), &result)
+		if err != nil {
+			log.Printf("json unmarshal failed . Error : %v", err)
+			continue
+		}
 
+		userKey := fmt.Sprintf("%d_%d", result.UserId, result.ProductId)
+		fmt.Println("userKey:", userKey)
+
+		//加锁
+		config.SkAppContext.UserConnMapLock.Lock()
+
+		resultChan, ok := config.SkAppContext.UserConnMap[userKey]
+		//解锁
+		config.SkAppContext.UserConnMapLock.Unlock()
+		if !ok {
+			log.Printf("user not found:%v", userKey)
+			continue
+		}
+		log.Printf("request result send to chan")
+		//channel验证通过，将结果放入channel
+		resultChan <- result
+		log.Printf("request result send to chan success,userKey:%v", userKey)
 	}
 }
