e225b5deb345c82003bab86c870898b020656406
tao <1216707759@qq.com>
2020-07-06 13:42:09 +0800

    metrics中间件

--- sk-app/plugins/meitrics.go
+++ sk-app/plugins/meitrics.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-kit/kit/metrics"
 	"secondkill/sk-app/model"
 	"secondkill/sk-app/service"
+	"time"
 )
 
 //metrics中间件
@@ -19,24 +20,58 @@ type skAppMetricMiddleware struct {
 }
 
 func (s *skAppMetricMiddleware) HealthCheck() bool {
-	panic("implement me")
+	defer func(begin time.Time) {
+		lvs := []string{
+			"method", "HealthCheck",
+		}
+		s.requestCount.With(lvs...).Add(1)
+		s.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	result := s.service.HealthCheck()
+	return result
 }
 
 func (s *skAppMetricMiddleware) SecInfo(productId int) (data map[string]interface{}) {
-	panic("implement me")
+	defer func(begin time.Time) {
+		lvs := []string{
+			"method", "SecInfo",
+		}
+		s.requestCount.With(lvs...).Add(1)
+		s.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+	}(time.Now())
+	data = s.service.SecInfo(productId)
+	return data
+
 }
 
 func (s *skAppMetricMiddleware) SecKill(req *model.SecRequest) (map[string]interface{}, int, error) {
-	panic("implement me")
+	defer func(begin time.Time) {
+		lvs := []string{
+			"method", "SecKill",
+		}
+		s.requestCount.With(lvs...).Add(1)
+		s.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+	}(time.Now())
+	result, code, err := s.service.SecKill(req)
+	return result, code, err
 }
 
 func (s *skAppMetricMiddleware) SecInfoList() ([]map[string]interface{}, int, error) {
-	panic("implement me")
+	defer func(begin time.Time) {
+		lvs := []string{
+			"method", "SecKill",
+		}
+		s.requestCount.With(lvs...).Add(1)
+		s.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+	}(time.Now())
+	data, code, err := s.service.SecInfoList()
+	return data, code, err
 }
 
 func NewSkAppMetrics(requestCount metrics.Counter, requestLatency metrics.Histogram) service.ServiceMiddleware {
 	return func(next service.Service) service.Service {
-		return skAppMetricMiddleware{
+		return &skAppMetricMiddleware{
 			service:        next,
 			requestCount:   requestCount,
 			requestLatency: requestLatency,
