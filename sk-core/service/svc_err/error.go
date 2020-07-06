33a4511b8594ff51fd779c2206e94bd8220d4ae7
tao <1216707759@qq.com>
2020-07-06 22:08:04 +0800

    error code

--- sk-core/service/svc_err/error.go
+++ sk-core/service/svc_err/error.go
@@ -6,12 +6,11 @@
 package svc_err
 
 const (
-	ErrServiceBusy     = 1001
-	SecKillSuccess     = 1002
-	ErrNotFoundProduct = 1003
-	ErrSoldOut         = 1004
-	ErrRetry           = 1005
-	ErrAlreadyBuy      = 1006
-
+	ErrServiceBusy       = 1001
+	SecKillSuccess       = 1002
+	ErrNotFoundProduct   = 1003
+	ErrSoldOut           = 1004
+	ErrRetry             = 1005
+	ErrAlreadyBuy        = 1006
 	ProductStatusSoldOut = 2001
 )
