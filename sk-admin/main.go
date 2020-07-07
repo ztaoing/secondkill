8c144728fa7f55e3b2601c290dfeff2f501f63b2
tao <1216707759@qq.com>
2020-07-07 22:31:46 +0800

    初始化 MySQL、zk、etcd、http

---
+++ sk-admin/main.go
@@ -0,0 +1,21 @@
+/**
+* @Author:zhoutao
+* @Date:2020/7/7 下午10:26
+ */
+
+package main
+
+import (
+	"secondkill/pkg/bootstrap"
+	pkgConfig "secondkill/pkg/config"
+	"secondkill/pkg/mysql"
+	"secondkill/sk-admin/setup"
+)
+
+func main() {
+	mysql.InitMysql(pkgConfig.MysqlConfig.Host, pkgConfig.MysqlConfig.Port, pkgConfig.MysqlConfig.User, pkgConfig.MysqlConfig.Pwd, pkgConfig.MysqlConfig.Db)
+
+	//setup.InitEtcd()
+	setup.InitZK()
+	setup.InitHTTP(bootstrap.HttpConfig.Host, bootstrap.HttpConfig.Port)
+}
