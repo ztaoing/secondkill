831cd6e4303d5013a228ecff6fda60dbd51d8d7b
tao <1216707759@qq.com>
2020-07-07 16:20:43 +0800

    product service操作

---
+++ sk-admin/service/product.go
@@ -0,0 +1,40 @@
+/**
+* @Author:zhoutao
+* @Date:2020/7/7 下午4:13
+ */
+
+package service
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"log"
+	"secondkill/sk-admin/model"
+)
+
+type ProductService interface {
+	CreateProduct(product *model.Product) error
+	GetProductList() ([]gorose.Data, error)
+}
+
+type ProductServiceImpl struct {
+}
+
+func (p ProductServiceImpl) CreateProduct(product *model.Product) error {
+	productEntity := model.NewProductModel()
+	err := productEntity.CreateProduct(product)
+	if err != nil {
+		log.Printf("productEntity CreateProduct,ERROR:%v", err)
+		return err
+	}
+	return nil
+}
+
+func (p ProductServiceImpl) GetProductList() ([]gorose.Data, error) {
+	productEntity := model.NewProductModel()
+	productList, err := productEntity.GetProductList()
+	if err != nil {
+		log.Printf("productEntity GetProductList,ERROR:%v", err)
+		return nil, err
+	}
+	return productList, nil
+}
